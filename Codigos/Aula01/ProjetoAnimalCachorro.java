66d01e216793848b51f6e8925fcea3b0f51010f1
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-07-29 12:21:36 -0300

    Create ProjetoAnimalCachorro.java

---
+++ Codigos/Aula01/ProjetoAnimalCachorro.java
@@ -0,0 +1,14 @@
+package projetoanimal;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Cachorro extends ProjetoAnimal{
+    @Override
+    void emitirSom() {
+        System.out.println("O cusco ta latindo: auauau");
+                
+                
+                
+    }
