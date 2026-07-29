944bbd8ff47ac02359be5e9856c66065102b0578
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-07-29 12:21:19 -0300

    Create ProjetoAnimalPrincipal.java

---
+++ Codigos/Aula01/ProjetoAnimalPrincipal.java
@@ -0,0 +1,14 @@
+package projetoanimal;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+        c.nome = "Caramelo";
+        c.exibirDados();
+        c.emitirSom();
+    }
+}
