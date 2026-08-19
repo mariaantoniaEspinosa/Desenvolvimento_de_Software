08daee0e2dab42f747507bd0efa68dc78a2e34e9
laboratorio <laboratorio@LAB01DT15.ACAD.UFN.EDU.BR>
2026-08-19 11:20:25 -0300

    fazendo atividade de aula

--- src/projetoaula03/Formulario02.java
+++ src/projetoaula03/Formulario02.java
@@ -16,6 +16,7 @@ public class Formulario02 extends javax.swing.JFrame {
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Formulario02.class.getName());
 
     public char sexo;
+    public boolean astronomia, tecnologia, esportes;
     
     public Formulario02() {
         initComponents();
@@ -39,6 +40,10 @@ public class Formulario02 extends javax.swing.JFrame {
         btnSalvar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         txaPessoa = new javax.swing.JTextArea();
+        chkTecnologia = new javax.swing.JCheckBox();
+        chkAstronomia = new javax.swing.JCheckBox();
+        chkEsportes = new javax.swing.JCheckBox();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -63,27 +68,48 @@ public class Formulario02 extends javax.swing.JFrame {
         txaPessoa.setRows(5);
         jScrollPane1.setViewportView(txaPessoa);
 
+        chkTecnologia.setText("Tecnologia");
+
+        chkAstronomia.setText("Astronomia");
+
+        chkEsportes.setText("esportes");
+
+        jLabel3.setText("jLabel3");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(67, 67, 67)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnSalvar)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(67, 67, 67)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(rdo_Masculino)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(rdo_Feminino))
-                            .addComponent(txtNome))))
-                .addContainerGap(190, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btnSalvar))
+                            .addComponent(txtNome)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(75, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jScrollPane1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(rdo_Feminino)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jLabel3)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(chkTecnologia)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(chkAstronomia)))
+                                .addGap(18, 18, 18)
+                                .addComponent(chkEsportes)))))
+                .addGap(65, 65, 65))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -93,21 +119,48 @@ public class Formulario02 extends javax.swing.JFrame {
                     .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
                 .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(rdo_Masculino)
+                        .addComponent(jLabel2))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(btnSalvar)
+                        .addComponent(rdo_Feminino)))
+                .addGap(43, 43, 43)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(rdo_Masculino)
-                    .addComponent(rdo_Feminino)
-                    .addComponent(jLabel2))
-                .addGap(18, 18, 18)
-                .addComponent(btnSalvar)
-                .addGap(30, 30, 30)
+                    .addComponent(chkTecnologia)
+                    .addComponent(chkAstronomia)
+                    .addComponent(chkEsportes)
+                    .addComponent(jLabel3))
+                .addGap(35, 35, 35)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(91, Short.MAX_VALUE))
+                .addContainerGap(62, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarActionPerformed
+        
+        if(chkTecnologia.isSelected()){
+            tecnologia = true;
+        }
+        else{
+            tecnologia = false;
+        }
+        if(chkAstronomia.isSelected()){
+            astronomia = true;
+        }
+        else{
+            astronomia = false;
+        }
+        if(chkEsportes.isSelected()){
+            esportes = true;
+        }
+        else{
+            esportes = false;
+        }
+        
         if(rdo_Masculino.isSelected()){
             sexo = 'M';
             Pessoa p = new Pessoa(txtNome.getText(), sexo, "Português");
@@ -153,8 +206,12 @@ public class Formulario02 extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup btnGrp_Sexo;
     private javax.swing.JButton btnSalvar;
+    private javax.swing.JCheckBox chkAstronomia;
+    private javax.swing.JCheckBox chkEsportes;
+    private javax.swing.JCheckBox chkTecnologia;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JRadioButton rdo_Feminino;
     private javax.swing.JRadioButton rdo_Masculino;
