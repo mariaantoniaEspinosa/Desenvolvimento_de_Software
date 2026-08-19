c23fdf647594b20d92385407c60bcb4ed1040b46
Maria Antonia <m.antonia.e.w@gmail.com>
2026-08-19 15:45:33 -0300

    "Começando a atividade semanal de Desenvolvimento de Software"

---
+++ Atividades/CadastroAlunos/src/cadastroalunos/Aluno.java
@@ -0,0 +1,63 @@
+package cadastroalunos;
+
+public class Aluno {
+    private String nomeCompleto;
+    private String dataNascimento;
+    private String sexo;
+    private int matricula;
+    private String curso;
+    private String cpf;
+    private String endereco;
+    private String estado;
+    private String telefone;
+
+    public Aluno(String nomeCompleto, String dataNascimento, String sexo, int matricula, String curso, String cpf, String endereco, String estado, String telefone) {
+        this.nomeCompleto = nomeCompleto;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.matricula = matricula;
+        this.curso = curso;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.estado = estado;
+        this.telefone = telefone;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+    
+    
+}
